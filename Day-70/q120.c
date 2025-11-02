0cbde588cb7da05a3ffc5bace9635a04c67184eb
vedantchouhan <chouhanvedant84@gmail.com>
2025-11-02 12:07:58 +0530

    code

---
+++ Day-70/q120.c
@@ -0,0 +1,44 @@
+/*
+Q120: Write a program to take a string input. Change it to sentence case.
+
+Sample Test Cases:
+Input 1:
+str = I am trying to build logic.
+Output 1:
+I Am Trying To Build Logic
+
+Input 2:
+str = The classes are supposed to start early.
+Output 2:
+The Classes Are Supposed To Start Early.
+
+Input 3:
+str = We are going to look at 26 different test cases.
+Output 3:
+We Are Going To Look At 26 Different Test Cases.
+*/
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char str[200];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    int capitalizeNext = 1;
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (capitalizeNext && isalpha(str[i])) {
+            str[i] = toupper(str[i]);
+            capitalizeNext = 0;
+        } else if (str[i] == ' ') {
+            capitalizeNext = 1;
+        } else {
+            str[i] = tolower(str[i]);
+        }
+    }
+
+    printf("Sentence Case: %s", str);
+    return 0;
+}
